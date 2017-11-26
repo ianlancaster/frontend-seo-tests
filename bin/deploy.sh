49e685b5506e0fe8b90b159c49fc227a04e46289
Ian Lancaster <ianclancaster@gmail.com>
2017-11-26 15:14:54 -0700

    Update deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 if git diff-index --quiet HEAD --; then # Check to make sure there are no uncommited changes
     set -o errexit; # Exit on error
-    # Save the current branch
-    branch_name=$(git symbolic-ref -q HEAD)
-    branch_name=${branch_name##refs/heads/}
-    branch_name=${branch_name:-HEAD}
     npm run clean;
     npm run build;
-    git add .;
-    git commit -m "Create new production build";
     git checkout -b gh-pages;
-    git merge branch_name;
+    find [path] -type -not -name 'dist' -not -name '.git' -delete
 else
   echo Please commit or stash your changes first.;
 fi
